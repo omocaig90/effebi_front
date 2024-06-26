4609566f174ae8d7e06b44152d8e884f55c5ee19
Giacomo Frola <giacomo.frola@gmail.com>
2024-06-26 18:49:41 +0200

    [feat]: Inizio configurazione Sito

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Benvenuto nella Home</h1>
+      <p>Questa è la home page del tuo sito.</p>
+    </div>
+  );
+}
+
+export default Home;
